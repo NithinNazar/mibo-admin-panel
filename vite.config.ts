65045e5c2e5c6dba18cb8d7b94af7004f6ec3710
NithinNazar <nithinanazar@gmail.com>
2026-01-30 19:24:08 +0530

    build error fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/admin/", // 👈 ADD THIS
   plugins: [react()],
   resolve: {
     alias: {
